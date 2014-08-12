194fa68a5350e0f23893aade04a45e7ff091f275
Kris Payne <kris@krispayne.com>
2014-08-12 13:19:56 -0700

    Update 109CIS.sh

--- 109CIS.sh
+++ 109CIS.sh
@@ -11,7 +11,7 @@
 ########################################################################
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.1094cis` has finished
+# Keep-alive: update existing `sudo` time stamp until `109CIS.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ########################################################################
