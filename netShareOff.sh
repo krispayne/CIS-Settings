4ace85886ce5a7e94bc15ae43aa5885af3321b20
krispayne <krispayne@me.com>
2014-12-23 14:39:58 -0800

    added absolute paths

--- netShareOff.sh
+++ netShareOff.sh
@@ -3,12 +3,12 @@
 # These options are all off by default and running the launchctl unload on them will begin crippling the system
 
 # 2.4.2 Disable Internet Sharing (Scored)
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.nat NAT -dict Enabled -int 0
-sudo launchctl unload -w /System/Library/LaunchDaemons/ com.apple.InternetSharing.plist
+sudo /usr/bin/defaults write /Library/Preferences/SystemConfiguration/com.apple.nat NAT -dict Enabled -int 0
+sudo /bin/launchctl unload -w /System/Library/LaunchDaemons/ com.apple.InternetSharing.plist
 
 # 2.4.8 Disable File Sharing (Scored)
-sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.AppleFileServer.plist
-sudo launchctl unload -w /System/Library/LaunchDaemons/ftp.plist
-sudo defaults delete /Library/Preferences/SystemConfiguration/com.apple.smb.server EnabledServices
-sudo launchctl unload -w /System/Library/LaunchDaemons/nmbd.plist
-sudo launchctl unload -w /System/Library/LaunchDaemons/smbd.plist
+sudo /bin/launchctl unload -w /System/Library/LaunchDaemons/com.apple.AppleFileServer.plist
+sudo /bin/launchctl unload -w /System/Library/LaunchDaemons/ftp.plist
+sudo /usr/bin/defaults delete /Library/Preferences/SystemConfiguration/com.apple.smb.server EnabledServices
+sudo /bin/launchctl unload -w /System/Library/LaunchDaemons/nmbd.plist
+sudo /bin/launchctl unload -w /System/Library/LaunchDaemons/smbd.plist
