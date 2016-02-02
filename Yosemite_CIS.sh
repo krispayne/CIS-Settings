3d7de39e61d5e9eafbf252fbaeeba89db5c1cc07
Kris Payne <krispayne@me.com>
2016-02-02 11:17:21 -0800

    remove sudo keep-alive
    this script should be run as root. it is ran as such when in a policy.

--- Yosemite_CIS.sh
+++ Yosemite_CIS.sh
@@ -319,11 +319,6 @@ mainScript() {
     
     # RUN AS ROOT
 
-    # SUDO UP, MF
-    #sudo -v
-    # Keep-alive: update existing `sudo` time stamp until the script has finished
-    #while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
     # comment out sections you do not want to run.
     softwareUpdates
     systemPreferences
